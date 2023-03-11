281ddd2c93f44078f15c66e221181febdc25c7c0
Mohamad Ehthesham S <62151491+MohamadFaiz0102@users.noreply.github.com>
2023-03-11 08:41:37 +0530

    Update Superstore Data Exploration Project using SQL.sql

--- superstore/Superstore Data Exploration Project using SQL.sql
+++ superstore/Superstore Data Exploration Project using SQL.sql
@@ -1,3 +1,27 @@
+-- Creating Table 
+CREATE TABLE superstore (
+  Row_ID        INTEGER  NOT NULL PRIMARY KEY ,
+  Order_ID      VARCHAR(14) NOT NULL,
+  Order_Date    DATE  NOT NULL,
+  Ship_Date     DATE  NOT NULL,
+  Ship_Mode     VARCHAR(14) NOT NULL,
+  Customer_ID   VARCHAR(8) NOT NULL,
+  Customer_Name VARCHAR(22) NOT NULL,
+  Segment       VARCHAR(11) NOT NULL,
+  Country       VARCHAR(13) NOT NULL,
+  City          VARCHAR(17) NOT NULL,
+  State         VARCHAR(20) NOT NULL,
+  Postal_Code   INTEGER  NOT NULL,
+  Region        VARCHAR(7) NOT NULL,
+  Product_ID    VARCHAR(15) NOT NULL,
+  Category      VARCHAR(15) NOT NULL,
+  Sub_Category  VARCHAR(11) NOT NULL,
+  Product_Name  VARCHAR(127) NOT NULL,
+  Sales         NUMERIC(9,4) NOT NULL
+);
+
+
+
 -- 1) What is the total sales amount for each category of products?
 
 SELECT 	
